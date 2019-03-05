90e34104dbe0e7a8aad5b2f81132869d7cd0812d
David M <support@smooth-systems.solutions>
2019-03-05 19:19:36 +0100

    updated spring boot version and build script

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 set -e
 
 mvn clean package
-docker build . -t smooth-systems-solutions/smtp-sink
-docker run -p 1025:10025 -p 8010:8010 --name smtp-sink smooth-systems-solutions/smtp-sink
+docker build . -t smoothsystemssolutions/smtp-server-mock
+#docker run -p 1025:10025 -p 8010:8010 --name smtp-server-mock smooth-systems-solutions/smtp-server-mock
 #docker run --rm -p 1025:10025 -p 8010:8010 --name smtp-sink smooth-systems-solutions/smtp-sink
 
 # docker exec -it smtp-sink bash
